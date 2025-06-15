d9a77085c84a3b6641de9b29d0d07bf18bb4c7be
KostebelovArtyom <kostebelovartem@yandex.ru>
2025-06-15 22:51:51 +0300

    Think about use DB for storage pass and write new func

--- storeman.py
+++ storeman.py
@@ -1,5 +1,6 @@
 import random
 import string
+import mysql.connector
 
 storage = {}
     
@@ -14,3 +15,20 @@ def adduser(username: str)  -> None:
     
 def remuser(username: str) -> None:
     storage.pop(username)
+
+def add_entry(link_res: str, login: str) -> None:
+    password = passwd_gen()
+    dbconfig = {'host':'127.0.0.1',
+                'user':'PassMan',
+                'password':'qwe123',
+                'database':'passmandb'}
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """insert into storage
+              (link, login, password)
+              values
+              (%s, %s, %s)"""
+    cursor.execute(_SQL, (link_res, login, password))
+    conn.commit()
+    cursor.close()
+    conn.close()
