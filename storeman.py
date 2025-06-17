f602bd180b6c1ea8436c1890aa726c952e2068e7
KostebelovArtyom <kostebelovartem@yandex.ru>
2025-06-18 02:20:30 +0300

    i used oop, yes. i am ga

--- storeman.py
+++ storeman.py
@@ -7,24 +7,28 @@ dbconfig = {'host':'127.0.0.1',
             'password':'qwe123',
             'database':'passmandb'}
 
-def passwd_gen():
-    total = string.ascii_letters + string.punctuation + string.digits
-    val = ''
-    for i in range(16):
-        val += secrets.choice(total)
-    return val
-    
-class StorContol:
-    def add_entry(link_res: str, login: str) -> None:
-        password = passwd_gen()
-        with UseDatabase(dbconfig) as cursor:
+class StoreContol:
+    def __init__(self, dbconfig):
+        self.dbconfig = dbconfig
+
+    @classmethod
+    def passwd_gen(self):
+        total = string.ascii_letters + string.punctuation + string.digits
+        val = ''
+        for i in range(16):
+            val += secrets.choice(total)
+        return val
+            
+    def add_entry(self, link_res: str, login: str) -> None:
+        password = self.passwd_gen()
+        with UseDatabase(self.dbconfig) as cursor:
             _SQL = """insert into storage
                       (link, login, password)
                       values
                       (%s, %s, %s)"""
             cursor.execute(_SQL, (link_res, login, password))
 
-    def rem_entry(ident: str) -> None:
-        with UseDatabase(dbconfig) as cursor:
+    def rem_entry(self, ident: int) -> None:
+        with UseDatabase(self.dbconfig) as cursor:
             _SQL = """DELETE FROM storage WHERE id = %s"""
             cursor.execute(_SQL, (ident,))
