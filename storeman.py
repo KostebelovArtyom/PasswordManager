f5a9cd3d3b6b27bb4f29c952a28b233c930e6a83
KostebelovArtyom <kostebelovartem@yandex.ru>
2025-06-18 01:05:25 +0300

    create class and move func with DB into it, del dict and same func

--- storeman.py
+++ storeman.py
@@ -2,35 +2,22 @@ import random
 import string
 from DBcm import UseDatabase
 
-storage = {}
-
 dbconfig = {'host':'127.0.0.1',
             'user':'PassMan',
             'password':'qwe123',
             'database':'passmandb'}
-    
-def passwd_gen() -> str:
-    total = string.ascii_letters + string.digits + string.punctuation
-    count = 18
-    result = ''.join(random.sample(total, count))
-    return result
 
-def adduser(username: str)  -> None:
-    storage[username] = passwd_gen()
-    
-def remuser(username: str) -> None:
-    storage.pop(username)
+class StorContol:
+    def add_entry(link_res: str, login: str) -> None:
+        password = passwd_gen()
+        with UseDatabase(dbconfig) as cursor:
+            _SQL = """insert into storage
+                      (link, login, password)
+                      values
+                      (%s, %s, %s)"""
+            cursor.execute(_SQL, (link_res, login, password))
 
-def add_entry(link_res: str, login: str) -> None:
-    password = passwd_gen()
-    with UseDatabase(dbconfig) as cursor:
-        _SQL = """insert into storage
-                  (link, login, password)
-                  values
-                  (%s, %s, %s)"""
-        cursor.execute(_SQL, (link_res, login, password))
-
-def rem_entry(ident: str) -> None:
-    with UseDatabase(dbconfig) as cursor:
-        _SQL = """DELETE FROM storage WHERE id = %s"""
-        cursor.execute(_SQL, (ident,))
+    def rem_entry(ident: str) -> None:
+        with UseDatabase(dbconfig) as cursor:
+            _SQL = """DELETE FROM storage WHERE id = %s"""
+            cursor.execute(_SQL, (ident,))
